7a239301e6c7b106f6ef185b7f2dbd1f324ae977
xrming <xrming@live.unc.edu>
2021-12-03 17:25:31 -0500

    test

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const info = stmt.run(req.params.id,req.body.user,md5(req.body.pass));
+	const info = stmt.run(req.body.user,md5(req.body.pass,req.params.id));
 	res.status(200).json({"message":info.changes+" record updated: ID "+req.params.id+ " (200)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
