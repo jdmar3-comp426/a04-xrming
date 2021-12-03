07f2a23be4d6b7c22639e5c1b3c09bd5ce8f682c
xrming <xrming@live.unc.edu>
2021-12-03 15:25:40 -0500

    still piazza change

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new/user", (req, res) => {
+app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user,md5(req.body.pass));
 	res.status(201).json({"message":info.changes+" record created: ID "+info.lastInsertRowid+ " (201)"    });
