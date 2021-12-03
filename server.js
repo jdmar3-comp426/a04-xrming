d211c79aa56a7ca1c137d08cd12c023d541cdda0
xrming <xrming@live.unc.edu>
2021-12-03 16:41:07 -0500

    fixed typo

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/app/user/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
 	const info = stmt.run(req.params.id,req.body.user,md5(req.body.pass));
-	res.status(200).json({"message":info.changes+" record updated: ID "+info.req.params.id+ " (201)"});
+	res.status(200).json({"message":info.changes+" record updated: ID "+req.params.id+ " (201)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
