13ecd0169c3c8b16d8d8988e8dbbb0b54a80453e
xrming <xrming@live.unc.edu>
2021-12-03 17:02:02 -0500

    deleted

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.patch("/app/update/user/:id", (req, res) => {
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
-	const info = stmt.run(req.params.id,req.body.user,req.body.pass);
-	res.status(200).json({"message":stmt.changes});
+	const info = stmt.run(req.params.id);
+	res.status(200).json({"message":info.changes+" record deleted: ID "+req.params.id});
 });
 // Default response for any other request
 app.use(function(req, res){
