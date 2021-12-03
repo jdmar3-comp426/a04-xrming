edbf7920ea368470617e28efb850c4769713948c
xrming <xrming@live.unc.edu>
2021-12-03 11:45:33 -0500

    first attempt

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-
+var db = require("./database.js")
 // Require md5 MODULE
-
+var md5 = require("md5")
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -23,21 +23,33 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/new/user"), (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)").run()
+	res.status(201).json(stmt);
+}
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
-app.get("/app/users", (req, res) => {	
+app.get("/app/users", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
 	res.status(200).json(stmt);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-
+app.get("/app/user/:id"), (req, res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get();
+	res.status(200).json(stmt);
+}
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/update/user/:id"), (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run();
+	res.status(200).json(stmt);
+}
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.delete("/app/delete/user/:id"), (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run();
+	res.status(200).json(stmt);
+}
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
