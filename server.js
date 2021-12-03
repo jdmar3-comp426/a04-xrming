b1e57207ef84120c511c39449259dfbed7eb58c4
xrming <xrming@live.unc.edu>
2021-12-03 12:43:27 -0500

    see if this works

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare('SELECT * FROM userinfo WHERE id = "${req.params.id}"').all();
-	res.status(200)
+	const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ? ').get(req.params.id);
+	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
