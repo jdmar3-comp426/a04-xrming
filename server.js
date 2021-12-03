8ea78587edac70fe49dc6e5a53c8c6671f76fa40
xrming <xrming@live.unc.edu>
2021-12-03 12:22:07 -0500

    test

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get();
-	res.status(200).json(stmt);
+	res.send('user ' + req.params.id)
+	res.status(200)
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
